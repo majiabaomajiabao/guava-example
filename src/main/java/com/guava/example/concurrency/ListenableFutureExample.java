4d277b38b1aab31a86eb7dfb16faf20cc37ad5e8
majiabao <majiabaoIT@163.com>
2018-01-03 23:15:21 +0800

    C

--- src/main/java/com/guava/example/concurrency/ListenableFutureExample.java
+++ src/main/java/com/guava/example/concurrency/ListenableFutureExample.java
@@ -1,7 +1,9 @@
 package com.guava.example.concurrency;
 
+import com.google.common.base.Function;
 import com.google.common.util.concurrent.*;
 
+import javax.annotation.Nullable;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 
@@ -11,11 +13,12 @@ import java.util.concurrent.Executors;
 public class ListenableFutureExample {
     public static void main(String[] args) {
 
-        //场景1
+        //1.callback
         ListeningExecutorService service = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(10));
         ListenableFuture<String> explosion = service.submit(new Callable<String>() {
             @Override
             public String call() {
+                System.out.println("a");
                 return "a";
             }
         });
@@ -33,16 +36,20 @@ public class ListenableFutureExample {
         });
         //内部实现还是通过executorService来实现的
         //submit使用的是前一句创建的executor,添加回调又是使用的另外一个executor
+        //在addCallback执行的过程中，会调用future.get方法，同步来获取值，但不会阻塞主线程，所以还是异步的
 
-        //场景2
-        ListenableFutureTask task = ListenableFutureTask.create(new Callable<Object>() {
 
+        //2.transform
+        ListenableFuture<String> transform = Futures.transform(explosion, new Function<String, String>() {
+            @Nullable
             @Override
-            public Object call() throws Exception {
-                return "a";
+            public String apply(@Nullable String input) {
+                String result =  "transfer" + input;
+                System.out.println(result);
+                return result;
             }
-        });
-
-        //
+        }, MoreExecutors.directExecutor());
+        //和callback逻辑相似，将explosion和function的操作封装成一个runnable整体，然后将最终的执行结果设置到结果中。
+        // 同样也是在transform的执行过程中，会调future.get
     }
 }
